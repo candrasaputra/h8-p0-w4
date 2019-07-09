b2a3acf9c35ca90c8e631ea2de785a348596a68f
candra saputra <candrasaputra@live.com>
2019-07-09 09:26:50 +0700

    change type data alfabet from array to string

--- Selasa/exercises-5.js
+++ Selasa/exercises-5.js
@@ -6,7 +6,7 @@
 */
 
 function ubahHuruf(kata) {
-    let alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let alfabet = 'abcdefghijklmnopqrstuvwxyz';
 
     let kataBaru = '';
 
